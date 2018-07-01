83f56106b795fd0d5ceac42641e28a58d0586552
stooged <stooged@users.noreply.github.com>
2018-07-01 12:18:53 +0800

    v4

--- build.sh
+++ build.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
+set -e
+pushd tool
+make
+popd
 pushd Cache_Install
 make
 popd
 mkdir -p bin
 rm -f bin/Cache_Install.bin
 cp Cache_Install/Cache_Install.bin bin/Cache_Install.bin
+mkdir -p html_payload
+tool/bin2js bin/Cache_Install.bin > html_payload/payload.js
+FILESIZE=$(stat -c%s "bin/Cache_Install.bin")
+PNAME=$"Cache Install"
+cp exploit.template html_payload/Cache_Install.html
+sed -i -f - html_payload/Cache_Install.html << EOF
+s/#NAME#/$PNAME/g
+s/#BUF#/$FILESIZE/g
+s/#PAY#/$(cat html_payload/payload.js)/g
+EOF
 rm -f Cache_Install/Cache_Install.bin
+rm -f html_payload/payload.js
--- clean.sh
+++ clean.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+pushd tool
+make clean
+popd
 pushd Cache_Install
 make clean
 popd
+rm -f html_payload/Cache_Install.html
 rm -f bin/Cache_Install.bin
+
